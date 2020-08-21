d2d4033fa7d77d7af3c8ec32eabab9bbc72d2fc0
satyam <satyam@netspring.io>
2020-08-20 23:42:37 -0700

    Add Emacs setup for ubuntu.

--- ubuntu_setup.sh
+++ ubuntu_setup.sh
@@ -13,3 +13,25 @@ sudo apt-get install git coreutils ispell clang-format tmux apt-file \
 echo "source ~/Projects/dotfiles/bashrc" >> ~/.bashrc
 ln -s $HOME/Projects/dotfiles/tmux.conf ~/.tmux.conf
 ln -s $HOME/Projects/dotfiles/vimrc ~/.vimrc
+
+# Install Emacs
+pushd ~/Projects
+git clone --single-branch --branch emacs-27 --depth 1 \
+    https://github.com/emacs-mirror/emacs.git
+cd emacs/
+sudo apt install -y autoconf make gcc texinfo libgtk-3-dev libxpm-dev \
+     libjpeg-dev libgif-dev libtiff5-dev libgnutls28-dev libncurses5-dev \
+     libjansson-dev libharfbuzz-dev libharfbuzz-bin
+./autogen.sh
+./configure --with-json --with-modules --with-harfbuzz \
+    --with-compress-install --with-threads --with-included-regex \
+    --with-zlib
+make -j12
+sudo make install
+
+# Setup dud
+cd ../
+git clone git@github.com:satyamshekhar/dud.git
+mkdir -p ~/.emacs.d/
+cp $HOME/Projects/dotfiles/emacs.init.el ~/.emacs.d/init.el
+ln -s $HOME/Projects/dud $HOME/.emacs.d/dud
