e04ec1c08bd214d3d636dc28e63d91cc38b21949
satyam <satyam@netspring.io>
2020-10-31 14:43:38 -0700

    Add sensible defaults for MacOs.

--- macos_setup.sh
+++ macos_setup.sh
@@ -38,3 +38,16 @@ ln -s ~/Projects/doom-emacs ~/.emacs.d
 ~/.emacs./bin/doom install
 rm -r ~/.doom.d
 ln -s ~/Projects/doom-emacs-config/ ~/.doom.d
+
+# Sensible MacOs defaults (https://github.com/mathiasbynens/dotfiles)
+osascript -e 'tell application "System Preferences" to quit'
+# Increase sound quality for Bluetooth headphones/headsets
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" \
+         -int 40
+# Enable full keyboard access for all controls (enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
